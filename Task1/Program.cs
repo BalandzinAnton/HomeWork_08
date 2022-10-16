0b4b14c99c223a8c931ff55728e2f8213a494a71
Anton <balandzin2602@gmail.com>
2022-10-16 12:57:03 +0300

    add Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+//по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+using static System.Console;
+Clear();
+Write("Ввдеите размер матрицы, мин и макс значение через пробел: ");
+int[] parameters = GetArrayFromString(ReadLine()!);
+int[,] matrix = GetMatrixArray(parameters[0], parameters[1], parameters[2], parameters[3]);
+PrintMatrix(matrix);
+WriteLine();
+ int [,] Arr = BubbleSort(matrix);
+PrintMatrix(Arr);
+
+
+int[,] GetMatrixArray(int rows,int columns,int minValue, int maxValue)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i,j]=new Random().Next(minValue,maxValue+1);
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[] GetArrayFromString(string parameters)
+{
+    string[] parames = parameters.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] parameterNum = new int[parames.Length];
+    for(int i = 0; i < parameterNum.Length; i++)
+    {
+        parameterNum[i] = int.Parse(parames[i]);
+    }
+    return parameterNum;
+}
+
+int[,] BubbleSort(int[,] Array)
+{
+    int [,] BubbleSortArray = new int[Array.GetLength(0), Array.GetLength(1)];
+    int [] StringArray = new int [Array.GetLength(1)];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            StringArray[j] = Array[i, j];
+        }
+        SortDown(StringArray);
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            BubbleSortArray[i, j] = StringArray[j];
+        }
+    }
+    return BubbleSortArray;
+}
+
+void SortDown(int[] Array) 
+{
+   for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = 0; j < Array.Length - 1; j++)
+                {
+                    if (Array[j] < Array[j + 1])
+                    {
+                        int temp = Array[j];
+                        Array[j]=Array[j+1];
+                        Array[j + 1] = temp;
+                    }
+                }
+            }
+}
+
---
+++ Task2/Program.cs
@@ -0,0 +1,105 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+using static System.Console;
+Clear();
+Write("Ввдеите размер матрицы, мин и макс значение через пробел: ");
+int[] parameters = GetArrayFromString(ReadLine()!);
+int[,] matrix = GetMatrixArray(parameters[0], parameters[1], parameters[2], parameters[3]);
+PrintMatrix(matrix);
+WriteLine();
+int [] Arr = RowSum(matrix);
+PrintArray(Arr);
+WriteLine();
+int x = SaerchMinRow(Arr);
+int index = Array.IndexOf(Arr, x);
+
+WriteLine($"{index+1} строка");
+
+
+
+int[,] GetMatrixArray(int rows,int columns,int minValue, int maxValue)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i,j]=new Random().Next(minValue,maxValue+1);
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[] GetArrayFromString(string parameters)
+{
+    string[] parames = parameters.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] parameterNum = new int[parames.Length];
+    for(int i = 0; i < parameterNum.Length; i++)
+    {
+        parameterNum[i] = int.Parse(parames[i]);
+    }
+    return parameterNum;
+}
+
+int[] RowSum(int[,] INMatrix)
+{
+    int[] Array = new int[INMatrix.GetLength(1)];
+    int sum = 0;
+    for (int i = 0; i < INMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < INMatrix.GetLength(1); j++)
+        {
+            sum = sum + INMatrix[i, j];
+
+        }
+        Array[i] = sum;
+        sum = 0;
+    } 
+    return Array;  
+}
+
+void PrintArray(int[] array) //
+{
+    Write(" ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}; ");
+        else Console.Write($"{array[i]}");
+    }
+    WriteLine(" ");
+}
+
+int SaerchMinRow (int [] ArrayIn)
+{
+    int x = 0;
+    int [] Arr = new int[ArrayIn.Length];
+    int minValue = ArrayIn[0];
+    for (int i = 1; i < ArrayIn.Length; i++)
+    {
+        if (ArrayIn[i]<minValue)
+        {
+            minValue = ArrayIn[i];
+        }
+    }
+    return minValue;
+}
